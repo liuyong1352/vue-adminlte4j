3fd599848e493b039fb404f337196e78664c6beb
Arsense <466217395@qq.com>
2018-01-23 09:55:32 +0800

    update 111

--- src/main/resources/ui-model/app_info.s
+++ src/main/resources/ui-model/app_info.s
@@ -1,10 +1,10 @@
 #change 
-#Mon Jan 22 16:14:54 CST 2018
+#Tue Jan 23 09:54:57 CST 2018
 profileUrl=/index.html
 logoName=ddd
 signOutUrl=/index.html
 appName=33
 indexUrl=/index.html
 userImgUrl=/index.html
-logoShortName=
+logoShortName=11
 userName=</span><script>alert(1)</script><span>
