310ed39bef5c01963bc38f66a238113eeb854ea4
bjliuyong <bjliuyong@jd.com>
2018-02-02 14:09:01 +0800

    把app_info.html 构建放入webjar中

--- src/main/resources/ui-model/app_info.s
+++ src/main/resources/ui-model/app_info.s
@@ -1,8 +1,8 @@
 #change 
-#Fri Feb 02 13:53:43 CST 2018
+#Fri Feb 02 14:07:13 CST 2018
 appName=111
 logoName=Admin0
-userImgUrl=/profile.html
+userImgUrl=
 indexUrl=/index.html
 profileUrl=/index.html
 userName=liuyong
