fa69e72f5fd2840f1fce00e77f20317caf61f4ef
bjliuyong <bjliuyong@jd.com>
2018-02-05 16:10:15 +0800

    把app_info.html 构建放入webjar中

---
+++ src/main/java/com/vue/adminlte4j/support/PageGenerator.java
@@ -0,0 +1,16 @@
+package com.vue.adminlte4j.support;
+
+/**
+ * Created by bjliuyong on 2018/2/5.
+ */
+public class PageGenerator {
+
+    public String title ;
+    public String path ;
+
+    public void gen() {
+
+
+
+    }
+}
