ed3d88de8ed5c6f016bc9fd16e2d925e5f820ba4
liuyong <bjliuyong@jd.com>
2018-02-12 14:17:57 +0800

    优化判断是否是开发时

--- src/main/java/com/vue/adminlte4j/support/PageGenerator.java
+++ src/main/java/com/vue/adminlte4j/support/PageGenerator.java
@@ -1,5 +1,10 @@
 package com.vue.adminlte4j.support;
 
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by bjliuyong on 2018/2/5.
  */
@@ -12,5 +17,13 @@ public class PageGenerator {
 
 
 
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        URL url = PageGenerator.class.getClassLoader().getResource("META-INF/sys_tpl_dft_config/starter.html");
+
+        List<String> content = Files.readAllLines(Paths.get(url.toURI())) ;
+        System.out.println(url);
     }
 }
