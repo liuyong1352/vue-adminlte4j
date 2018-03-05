ee2030c041fb9518d7f37d2a1eddc2c6dc9de37c
liuyong <bjliuyong@jd.com>
2018-03-05 14:36:25 +0800

    modify road map

--- src/test/java/api/IndexController.java
+++ src/test/java/api/IndexController.java
@@ -1,5 +1,8 @@
 package api;
 
+import api.data.MenuApiInJvm;
+import com.vue.adminlte4j.model.UIModel;
+import com.vue.adminlte4j.web.config.MenuConfig;
 import com.vue.adminlte4j.web.springmvc.ApiAdminController;
 import org.springframework.stereotype.Controller;
 
@@ -10,6 +13,26 @@ import org.springframework.stereotype.Controller;
 public class IndexController extends ApiAdminController {
 
 
+    /**
+     * config menu
+     * @param uiModel
+     */
+    public void configureMenu(UIModel uiModel) {
+        addSystemMenu(uiModel);
+        /**
+         * 完全自定义
+         */
+        //customMenu(uiModel) ;
+    }
 
+    private void addSystemMenu(UIModel uiModel) {
+        super.configureMenu(uiModel) ;
+        uiModel.menu(MenuApiInJvm.getMenu()) ;
+    }
 
+    private void customMenu(UIModel uiModel) {
+        uiModel.menu(MenuConfig.systemMenu())
+            .menu(MenuApiInJvm.getMenu()) ;
+
+    }
 }
