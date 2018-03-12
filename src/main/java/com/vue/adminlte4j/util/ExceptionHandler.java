c7279cdd4bca0378eea01c2eeeddc345fd4f5139
liuyong <bjliuyong@jd.com>
2018-03-12 11:25:28 +0800

    优化动态生成form表单

--- src/main/java/com/vue/adminlte4j/util/ExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.vue.adminlte4j.util;
-
-/**
- * Created by bjliuyong on 2018/3/12.
- */
-public class ExceptionHandler {
-}
--- src/main/java/com/vue/adminlte4j/web/springmvc/ApiAdminController.java
+++ src/main/java/com/vue/adminlte4j/web/springmvc/ApiAdminController.java
@@ -9,12 +9,16 @@ import com.vue.adminlte4j.support.ModelConfigManager;
 import com.vue.adminlte4j.util.MenuUtils;
 import com.vue.adminlte4j.util.Utils;
 import com.vue.adminlte4j.web.config.AdminApiConfig;
-import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by bjliuyong on 2018/2/2.
