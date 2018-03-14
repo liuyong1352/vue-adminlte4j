c838161dc30d608cd92369c339e3d67d305cd9a6
liuyong <bjliuyong@jd.com>
2018-03-14 18:30:59 +0800

    封装treeview

--- src/main/java/com/vue/adminlte4j/model/ITreeNode.java
+++ src/main/java/com/vue/adminlte4j/model/ITreeNode.java
@@ -1,10 +1,20 @@
 package com.vue.adminlte4j.model;
 
+import java.util.List;
+
 public interface ITreeNode {
 
     String getId() ;
 
     String getPid() ;
+
+    String getIcon() ;
+
+    String getText() ;
+
+    default List<String> getTags() {
+        return  null ;
+    }
 
     /**
      * 判断是否是根节点
