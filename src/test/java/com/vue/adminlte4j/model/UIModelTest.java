1be95a4154dacc61be4a4ff86ce4b0872a4c62bf
liuyong <bjliuyong@jd.com>
2018-03-20 15:43:47 +0800

    增加用户名展示的接入点

--- src/test/java/com/vue/adminlte4j/model/UIModelTest.java
+++ src/test/java/com/vue/adminlte4j/model/UIModelTest.java
@@ -1,26 +1,11 @@
 package com.vue.adminlte4j.model;
 
-import java.util.concurrent.locks.ReentrantLock;
 import org.junit.Test;
 
 /**
  * Created by bjliuyong on 2018/3/1.
  */
 public class UIModelTest {
-
-
-    private final ReentrantLock lock = new ReentrantLock();
-
-
-    public void m() {
-        lock.lock(); // block until condition holds
-        try {
-            // ... method body
-        }finally {
-            lock.unlock();
-        }
-    }
-
 
     @Test
     public void testToJson() {
