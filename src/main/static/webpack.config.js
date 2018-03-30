c437f0d65997d6bb6a9caf7ed4aae86343b0b947
liuyong <bjliuyong@jd.com>
2018-03-30 14:55:46 +0800

    优化 bootstrap-treeview依赖bower

--- src/main/static/webpack.config.js
+++ src/main/static/webpack.config.js
@@ -2,7 +2,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 var p = path.resolve(__dirname, '../')
 const lib_names = ['jquery/dist' , 'admin-lte/dist' , 'bootstrap/dist' , 'font-awesome/css' ,
-    'font-awesome/fonts' , 'select2/dist' ,'vue/dist' , 'axios/dist' ,'bootstrap-treeview/dist' ,
+    'font-awesome/fonts' , 'select2/dist' ,'vue/dist' , 'axios/dist' ,
     'datatables.net/js'  ,'layui-src/dist']
 const copyLibDir = []
 for(var index in lib_names) {
