cbae03d92ae5fd661c325291c22cf24bf0dd9b87
liuyong <bjliuyong@jd.com>
2018-04-12 18:39:45 +0800

    重构表单代码 主要提取可复用的代码

--- src/main/static/src/components/form/form-item.vue
+++ src/main/static/src/components/form/form-item.vue
@@ -1,28 +1,24 @@
 <template>
     <div class="layui-form-item">
         <label v-if="label" class="layui-form-label">{{label}}</label>
-        <div class="layui-input-block">
+        <div v-if="$slot" class="layui-input-block">
           <slot></slot>
         </div>
     </div>
 </template>
 
 <script>
-
 export default {
-  name: 'v-form-item',
-  props: {
-    span : {type : Number  , default : 12} ,
-    label : {type : String } ,
-    data: String
-  } ,
-  computed : {
-    col_size : function() {
-        return 'col-md-' + this.span
+    name: 'v-form-item',
+    props: {
+      span : {type : Number  , default : 12} ,
+      label : {type : String } ,
+      data: String
+    } ,
+    computed : {
+      col_size : function() {
+          return 'col-md-' + this.span
+      }
     }
-  } ,
-  mounted : function() {
-    console.log("form + mounted!!")
-  }
 }
 </script>
