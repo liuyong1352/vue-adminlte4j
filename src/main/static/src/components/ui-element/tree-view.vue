6860baa95980e1b0614d04c1e245cee04653d864
liuyong <bjliuyong@jd.com>
2018-05-08 15:46:07 +0800

    remove unused textarea

--- src/main/static/src/components/ui-element/tree-view.vue
+++ src/main/static/src/components/ui-element/tree-view.vue
@@ -1,59 +1,63 @@
 <template>
-    <div :id="id"></div>
+    <div :id="dynId"></div>
 </template>
 <script>
 export default {
-  name: 'v-tree-view',
-  props: {
-    id:{type:String , default:'tree'} ,
-    ajax_url:{type:String} ,
-    expand_icon : {type:String ,default :"glyphicon glyphicon-stop"} ,
-    collapse_icon : {type:String ,default :"glyphicon glyphicon-unchecked"}
-  } ,
-  methods: {
-
-    refresh : function(data) {
-        var self = this
-        if(data) {
-            refresh_data(response.data.treeData)
-            return
-        }
-        axios.get(this.ajax_url).then(function(response){
-            self.refresh_data(response.data.treeData)
-        })
-    },
-    refresh_data: function(_data) {
-        var self = this
-        var tree = $("#" + this.id).data('treeview')
-        var expanded= (tree && tree.getExpanded()) || []
-
-        $("#" + this.id).treeview({
-            data : _data,
-            showBorder : true,
-            expandIcon : self.expand_icon,
-            collapseIcon : self.collapse_icon,
-            levels : 1,
-            onNodeSelected : function(event, data) {
-                if(self.$listeners['on-node-selected'])
-                    self.$emit("on-node-selected", data)
+    name: 'v-tree',
+    props: {
+        id:{type:String} ,
+        ajax_url:{type:String} ,
+        expand_icon : {type:String ,default :"glyphicon glyphicon-stop"} ,
+        collapse_icon : {type:String ,default :"glyphicon glyphicon-unchecked"}
+    } ,
+    methods: {
+        refresh : function(data) {
+            var self = this
+            if(data) {
+                refresh_data(response.data.treeData)
+                return
             }
+            axios.get(this.ajax_url).then(function(response){
+                self.refresh_data(response.data.treeData)
+            })
+        },
+        refresh_data: function(_data) {
+            var self = this
+            var tree = $("#" + this.dynId).data('treeview')
+            var expanded= (tree && tree.getExpanded()) || []
 
-        })
+            $("#" + this.dynId).treeview({
+                data : _data,
+                showBorder : true,
+                expandIcon : self.expand_icon,
+                collapseIcon : self.collapse_icon,
+                levels : 1,
+                onNodeSelected : function(event, data) {
+                    if(self.$listeners['on-node-selected'])
+                        self.$emit("on-node-selected", data)
+                }
 
-        for(var i = 0 ; i<expanded.length ;i++){
-            var eid = expanded[i].nodeId
-            $("#" + this.id).data('treeview').expandNode(eid)
-            $("#" + this.id).data('treeview').revealNode(eid)
+            })
+
+            for(var i = 0 ; i<expanded.length ;i++){
+                var eid = expanded[i].nodeId
+                $("#" + this.dynId).data('treeview').expandNode(eid)
+                $("#" + this.dynId).data('treeview').revealNode(eid)
+            }
+                        //selectNodeId=selectNodeId||0;
+                        //$("#tree").data('treeview').selectNode(selectNodeId)
+                        //$("#tree").data('treeview').expandNode(selectNodeId)
+                        //$("#tree").data('treeview').revealNode(selectNodeId)
+                        //$('#' + this.dynId).on('nodeSelected', callback);
         }
-                    //selectNodeId=selectNodeId||0;
-                    //$("#tree").data('treeview').selectNode(selectNodeId)
-                    //$("#tree").data('treeview').expandNode(selectNodeId)
-                    //$("#tree").data('treeview').revealNode(selectNodeId)
-                    //$('#' + this.id).on('nodeSelected', callback);
+    },
+    computed:{
+        dynId() {
+            return (this.id || '_tree_' + unique_id())
+        }
+    } ,
+    mounted : function() {
+      this.refresh()
     }
-  } ,
-  mounted : function() {
-    this.refresh()
-  }
 }
 </script>
