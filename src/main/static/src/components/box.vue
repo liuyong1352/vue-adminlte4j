95ef8e084fb2482c78aa665b7359788ae558b192
Arsense <466217395@qq.com>
2018-09-20 23:28:20 +0800

    box OK

--- src/main/static/src/components/box.vue
+++ src/main/static/src/components/box.vue
@@ -1,56 +1,69 @@
 <template>
-  <div class="box">
-    <div class="box-header" :class="(isBorder || isSolid)?'with-border':''">
-        <slot name="icon-title"></slot>
-        <template v-if="title">
-            <h3 class="box-title">{{ title }}</h3>
-        </template>
-        <template v-if="$slots.header">
-            <slot name="header"></slot>
-        </template>
-        <div  class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" :data-widget="widgetType"><i :class="btnIcon"></i>
+    <div :class="classes" v-if="!removed" >
+        <div class="box-header" :class="(isBorder || is_solid)?'with-border':''">
+            <slot name="icon-title"></slot>
+            <h3 v-if="title" class="box-title" v-html="title"></h3>
+            <template v-if="$slots.header">
+                <slot name="header"></slot>
+            </template>
+            <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" @click="toggle">
+                    <i :class="state?'fa fa-minus':'fa fa-plus'"></i>
                 </button>
+            </div>
         </div>
-        <!-- /.box-tools -->
-    </div>
+        <div class="box-body">
+            <slot></slot>
+        </div>
 
-    <div class="box-body">
-          <slot></slot>
+        <div v-if="$slots.footer" class="box-footer">
+            <slot name="footer"></slot>
+        </div>
+        <div v-if="loading" class="overlay"><div class="fa fa-refresh fa-spin"></div></div>
     </div>
-    <!-- /.box-body -->
-    <div v-if="$slots.footer" class="box-footer">
-          <slot name="footer"></slot>
-    </div>
-  </div>
-
 </template>
-
 <script>
-export default {
-  name: 'v-box',
-  props: {
-      widgetType:   { type: String, default: 'collapse' },
-      title:        { type: String },
-      isSolid:      { type: Boolean, default: false },
-      isOpen:       { type: Boolean , default:true } ,
-      isBorder:     { type: Boolean, default: false }
-  },
-  mounted: function() {
-     $('.box').boxWidget()
-  },
-  computed: {
-      btnIcon () {
-        if (this.widgetType === 'collapse') {
-          return this.isOpen ? 'fa fa-minus' : 'fa fa-plus'
-        } else if (this.widgetType === 'remove') {
-          return 'fa fa-times'
+    export default {
+        name: 'v-box',
+        props: {
+            title:{ type: String },
+            is_solid:   { type: Boolean, default: false },
+            isBorder:  { type: Boolean, default: false } ,
+            theme: {type:String}
+        },
+        data() {
+            return {
+                state:1 ,
+                loading:0 ,
+                removed:0
+            }
+        } ,
+        methods: {
+            expand() {
+                $(this.$el).children('.box-body ,.box-footer' ).slideDown(500)
+            } ,
+            collapse() {
+                $(this.$el).children('.box-body ,.box-footer' ).slideUp(500)
+            } ,
+            toggle() {
+                this.state ^= 1
+                if(this.state) {
+                    this.expand()
+                } else {
+                    this.collapse()
+                }
+            } ,
+            remove() {
+                this.removed = 1
+            }
+        } ,
+        computed : {
+            classes() {
+                return ['box' , {
+                    [`box-${this.theme}`]:this.theme ,
+                    [`box-solid`]: this.is_solid
+                }]
+            }
         }
-        return ''
-      }
-  },
-  created () {
-
-  }
-}
+    }
 </script>
