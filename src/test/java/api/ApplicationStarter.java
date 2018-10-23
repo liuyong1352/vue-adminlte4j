ef98b0602a56dcc182208703220c0bf7ce1a4d22
Arsense <466217395@qq.com>
2018-10-23 15:20:42 +0800

    序列化优化更新同步

--- src/test/java/api/ApplicationStarter.java
+++ src/test/java/api/ApplicationStarter.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.vue.adminlte4j.support.FileChangeListener;
-import com.vue.adminlte4j.web.StaticFileFilter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -13,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
